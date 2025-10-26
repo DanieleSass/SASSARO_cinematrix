51019ded7d315095441af3be0fce0bce712761c1
DanieleSass <12999345@itisrossi.vi.it>
2025-10-26 17:55:06 +0100

    Fare check finale

--- main.c
+++ main.c
@@ -9,11 +9,11 @@ typedef struct {
     float durata;
 } Film;
 
-static char *const nome_file = "../film.dat";
+static char *const nome_file = "../catalogo.dat";
 
 Film* RichiediFilm();
 void StampaFilm(Film**,int);
-void RicercaPerTItolo(Film**,int);
+void RicercaPerTitolo(Film**, int);
 void OrdinaPerAnno(Film**, int);
 
 int file_esiste();
@@ -49,6 +49,7 @@ int main(void) {
     do {
         printf("1 Aggiungi Film\n2 Visualizza Catalogo\n3 Cerca Film per TItolo\n4 Ordina Libreria per anno\n5 Salva ed Esci\nScelta:");
         scanf("%d",&scelta);
+        getchar();
 
     switch (scelta) {
 
@@ -74,7 +75,7 @@ int main(void) {
             StampaFilm(lista,cont);
             break;
         case 3:
-            RicercaPerTItolo(lista,cont);
+            RicercaPerTitolo(lista,cont);
             break;
         case 4:
             OrdinaPerAnno(lista,cont);
@@ -82,9 +83,9 @@ int main(void) {
         case 5:
             salva_file(lista,cont);
             printf("Ciao\n");
-            exit(1);
+            break;
         default:
-            printf("Errore scelta.\n");
+            printf("Errore scelta\n");
             break;
     }
     }while (scelta!=5);
@@ -103,17 +104,43 @@ int main(void) {
 
 Film* RichiediFilm()
 {
-    Film *film;
+    Film *film=malloc(sizeof(Film));
 
-    printf("Nome Film");
-    fgets(film->regista, 30, stdin);
-    printf("Nome regista");
-    fgets(film->titolo, 30, stdin);
-    printf("Anno di pubblicazione");
-    scanf("%d",&film->anno);
-    printf("Durata");
-    scanf("%f",&film->durata);
+    film->titolo = malloc(50 * sizeof(char));
+    film->regista = malloc(50 * sizeof(char));
 
+    printf("Nome Film\n");
+    fgets(film->titolo, 50, stdin);
+
+    printf("Nome regista\n");
+    fgets(film->regista, 50, stdin);
+
+    do
+    {
+        printf("Anno di pubblicazione: \n");
+        if (scanf("%d", &film->anno) != 1) {
+            printf("Errore\n");
+            while (getchar() != '\n'); // pulisce il buffer
+        } else {
+            getchar(); // <-- pulisce il '\n' rimasto
+            break; // input corretto → esci
+        }
+    }while (1);
+
+    do {
+        printf("Durata: \n");
+        if (scanf("%f", &film->durata) != 1) {
+            printf("Errore\n");
+            while (getchar() != '\n'); // pulisce il buffer
+        } else {
+            getchar(); // <-- pulisce il '\n' rimasto
+            break; // input corretto → esci
+        }
+    }while (1);
+
+
+    film->titolo[strcspn(film->titolo, "\n")] = '\0';
+    film->regista[strcspn(film->regista, "\n")] = '\0';
     return film;
 }
 
@@ -126,29 +153,31 @@ void StampaFilm(Film** lista,int cont)
     printf("Lista:\n");
     for (int i=0;i<cont;i++)
     {
-        printf("Nome: %s Regista: %s Anno Pubblicazione: %d Durata: %f\n",lista[i]->titolo,lista[i]->titolo,lista[i]->titolo,lista[i]->titolo);
+        printf("Nome: %s Regista: %s Anno Pubblicazione: %d Durata: %f\n",lista[i]->titolo,lista[i]->regista,lista[i]->anno,lista[i]->durata);
     }
 }
 
-void RicercaPerTitolo(Film** lista, int cont) {
+void RicercaPerTitolo(Film** lista, int cont)
+{
 
     if (cont==0) {
         printf("Nessun film in lista\n");
         return;
     }
 
-    printf("Provvedi a scrivere una parola per vedere i film che la contengono nel loro titolo");
+    printf("Provvedi a scrivere una parola per vedere i film che la contengono nel loro titolo\n");
     char parola[50];
     fgets(parola,50,stdin);
-
+    parola[strcspn(parola, "\n")] = 0;
     for (int i=0;i<cont;i++) {
         if (strstr(lista[i]->titolo,parola) != NULL) {
-            printf("Titolo: %s Regista: %s", lista[i]->titolo, lista[i]->regista);
+            printf("Titolo: %s  Regista: %s\n", lista[i]->titolo, lista[i]->regista);
         }
     }
 }
 
-void OrdinaPerAnno(Film** lista, int cont) {
+void OrdinaPerAnno(Film** lista, int cont)
+{
     Film *temp;
     for (int i=0;i<cont-1;i++)
     {
@@ -166,7 +195,8 @@ void OrdinaPerAnno(Film** lista, int cont) {
 }
 
 ///////////////////////////   FILE //////////////////////////
-int file_esiste() {
+int file_esiste()
+{
     FILE* file=fopen(nome_file,"r");
     if (file)
     {
@@ -176,32 +206,62 @@ int file_esiste() {
         return 0;   //NON trovato
 }
 
+
 Film** leggi_file(int* dim) {
-    FILE* file_bin=fopen(nome_file,"rb");
-    if (file_bin==NULL) {
+    FILE* file_bin;
+    fopen(nome_file,"rb");
+    if (file_bin==NULL)
+    {
         printf("Errore apertura file");
         return NULL;
     }
 
     fread(dim,sizeof(int),1,file_bin);
+
     Film** listone=malloc(sizeof(Film*) * (*dim));
-    if (listone==NULL) {
+    if (listone==NULL)
+    {
         printf("Errore allocazione memoria durante lettura file");
         fclose(file_bin);
         return 0;
     }
-    fread(listone,sizeof(Film*),*dim,file_bin);
+
+    for (int i = 0; i < *dim; i++)
+    {
+        listone[i] = malloc(sizeof(Film));
+        listone[i]->titolo = malloc(50);
+        listone[i]->regista = malloc(50);
+
+        fread(listone[i]->titolo, sizeof(char), 50, file_bin);
+        fread(listone[i]->regista, sizeof(char), 50, file_bin);
+        fread(&listone[i]->anno, sizeof(int), 1, file_bin);
+        fread(&listone[i]->durata, sizeof(float), 1, file_bin);
+
+    }
     fclose(file_bin);
     return listone;
 }
 
-void salva_file(Film** lista,int dim) {
-    FILE* filebin=fopen(nome_file,"wb");
-    if (filebin==NULL) {
-        printf("Errore apertura file durante scrittura");
-        return;
+
+
+    void salva_file(Film** lista,int dim){
+        OrdinaPerAnno(lista,dim);
+        FILE* filebin=fopen(nome_file,"wb");
+        if (filebin==NULL) {
+            printf("Errore apertura file durante scrittura");
+            return;
+        }
+
+        fwrite(&dim,sizeof(int),1,filebin);
+
+        for (int i=0; i <dim;i++)
+        {
+            fwrite(lista[i]->titolo,sizeof(char),50,filebin);
+            fwrite(lista[i]->regista,sizeof(char),50,filebin);
+            fwrite(&lista[i]->anno, sizeof(int), 1, filebin);
+            fwrite(&lista[i]->durata, sizeof(float), 1, filebin);
+        }
+
+
+        fclose(filebin);
     }
-    fwrite(&dim,sizeof(int),1,filebin);
-    fwrite(lista,sizeof(Film*),dim,filebin);
-    fclose(filebin);
-}
